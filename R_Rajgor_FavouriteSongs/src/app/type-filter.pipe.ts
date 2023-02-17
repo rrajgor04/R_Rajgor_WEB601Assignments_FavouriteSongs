877951c5c89c609550fcb569bca964101beab81a
rrajgor04 <rr241@myscc.ca>
2023-02-17 17:24:07 -0500

    edited type filter pipe..

--- R_Rajgor_FavouriteSongs/src/app/type-filter.pipe.ts
+++ R_Rajgor_FavouriteSongs/src/app/type-filter.pipe.ts
@@ -1,12 +1,17 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { Content } from './helper-files/content-interface';
 
 @Pipe({
   name: 'typeFilter'
 })
 export class TypeFilterPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(contents: Content[], typeFilter: string): Content[] {
+    if(!typeFilter){
+      return contents.filter(content => !content.type);
+    }
+
+    return contents.filter(content => content.type === typeFilter);
   }
 
 }
