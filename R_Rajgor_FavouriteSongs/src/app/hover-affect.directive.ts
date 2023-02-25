ec0bb6e32441d81f80317f7f32bf3c229c2d5124
rrajgor04 <rr241@myscc.ca>
2023-02-24 21:00:49 -0500

    Added underline on types

--- R_Rajgor_FavouriteSongs/src/app/hover-affect.directive.ts
+++ R_Rajgor_FavouriteSongs/src/app/hover-affect.directive.ts
@@ -1,10 +1,29 @@
-import { Directive } from '@angular/core';
+import { Directive, HostListener, ElementRef, Input } from '@angular/core';
 
 @Directive({
   selector: '[appHoverAffect]'
 })
 export class HoverAffectDirective {
 
-  constructor() { }
+  @Input('appHoverAffect') hoverAffect: string = '';
+
+  constructor(private elm: ElementRef) { }
+
+  @HostListener('mouseenter') onMouseEnter(){
+    if(this.hoverAffect === 'underline'){
+      this.elm.nativeElement.style.textDecoration = this.hoverAffect;
+    }
+    else if(this.hoverAffect === 'bold'){
+      this.elm.nativeElement.style.fontWeight = this.hoverAffect;
+    }
+  }
+  @HostListener('mouseleave') onMouseLeave(){
+    if(this.hoverAffect === 'underline'){
+      this.elm.nativeElement.style.textDecoration = 'none';
+    }
+    else if(this.hoverAffect === 'bold'){
+      this.elm.nativeElement.style.fontWeight = 'normal';
+    }
+  }
 
 }
