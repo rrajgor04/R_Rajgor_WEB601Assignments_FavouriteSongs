78b2d0da4599489e291c1d349d57f068d724af17
rrajgor04 <rr241@myscc.ca>
2023-04-14 16:50:06 -0400

    added routes for list and detail

--- R_Rajgor_FavouriteSongs/src/app/app-routing.module.ts
+++ R_Rajgor_FavouriteSongs/src/app/app-routing.module.ts
@@ -1,12 +1,30 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { ContentListComponent } from './content-list/content-list.component';
+import { ContentDetailComponent } from './content-detail/content-detail.component';
 
-
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/list',
+    pathMatch: 'full'
+  },
+  {
+    path: '/list',
+    component: ContentListComponent
+  },
+  {
+    path: 'list/:id',
+    component: ContentDetailComponent
+  }
+]
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forRoot(routes)
   ]
 })
 export class AppRoutingModule { }
