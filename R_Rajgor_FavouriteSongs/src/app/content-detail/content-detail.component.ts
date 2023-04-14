6e477d75c9795f8c5fae323b01b766765877057b
rrajgor04 <rr241@myscc.ca>
2023-04-14 17:35:01 -0400

    details added in component

--- R_Rajgor_FavouriteSongs/src/app/content-detail/content-detail.component.ts
+++ R_Rajgor_FavouriteSongs/src/app/content-detail/content-detail.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { SongService } from '../services/song.service';
 
 @Component({
   selector: 'app-content-detail',
@@ -6,5 +8,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./content-detail.component.scss']
 })
 export class ContentDetailComponent {
+  id: number = 0;
+  content: any = {};
+  defaultSongImage: string = 'https://cdn.saleminteractivemedia.com/shared/images/default-cover-art.png';
 
+  constructor(private route:ActivatedRoute, private songsService: SongService){}
+
+  loggingIDnTitle(content: any){
+    console.log(`ID: ${content.id}`);
+    console.log(`Title: ${content.title}`);
+  }
+
+  ngOnInit(){
+    this.route.paramMap.subscribe(params => {
+      this.id = Number(params.get('id') ?? 0);
+
+      this.songsService.getSongById(this.id).subscribe(song => {
+        this.content = song;
+      });
+    })
+  }
 }
