b229610cb2e1f00f146e0dfaa4f7d6ac7e9b0163
rrajgor04 <rr241@myscc.ca>
2023-04-21 16:43:12 -0400

    installed service worker and using http-server to run our pwa

--- R_Rajgor_FavouriteSongs/src/app/app.module.ts
+++ R_Rajgor_FavouriteSongs/src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
@@ -26,6 +26,7 @@ import { ContentDetailComponent } from './content-detail/content-detail.componen
 
 import { AppRoutingModule } from './app-routing.module';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 @NgModule({
   declarations: [
@@ -55,7 +56,13 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
       delay: 1000
     }),
     BrowserAnimationsModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
